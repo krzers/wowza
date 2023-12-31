9d9d6d877498cd70692f91db7aa4574a1bfd4d28
krzers <tyhffggfg@gmail.com>
2023-12-31 18:35:20 +0700

    marla

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -2,22 +2,25 @@
 using namespace std;
 #include<ctime>
 
-long long int fibonacci(int);
+int gcd(int,int);
 
 int main(){
-    clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
-    return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
 }
 
-long long int fibonacci(int x){
-    if (x > 1){
-    return fibonacci(x-1) + fibonacci(x-2);
+
+int gcd(int x,int y){
+    if(x%y == 0){
+        return y;
     }
     else{
-    return x;
+        return gcd(y, x%y);
     }
-}
+    }
+
+
+
+
